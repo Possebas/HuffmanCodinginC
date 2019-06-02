7c8f94547194ae18552e643be69c6f02ad823a14
Gustavo <gustavo.possebon@acad.pucrs.br>
2019-06-02 00:18:48 -0300

    Tabela de caractere e frequência
    Tabela de caractere e frequência

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    char entrada[] = "aaaaabbbbcccdde";
+    int* numberChars = calloc(255, sizeof numberChars);
+
+    printf("Caracteres da mensagem em decimal:\n");
+    for(int i=0; i<strlen(entrada); i++){
+        printf(" [%d] ", entrada[i]);
+        numberChars[(int)entrada[i]] += 1;
+    }
+    //printf("\n\nVezes que cada caractere apareceu:\n");
+    /*
+    for(int i=0; i<255; i++){
+        if(numberChars[i] > 0){
+        printf(" [%d] ", numberChars[i]);
+        }
+    }
+    */
+    printf("\n\n");
+    printf("Caractere em ASCII          Quantidade");
+    for(int i=0;i<256;i++){
+        if(numberChars[i] > 0){
+            printf("\n%c                              %d\n", i, numberChars[i]);
+        }
+    }
+
+    //printf("\nHello world!\n");
     return 0;
 }
